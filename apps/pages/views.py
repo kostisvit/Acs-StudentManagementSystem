56ef07ebe6f8a1af43de048287ce3aaa2468e57d
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-03-22 09:40:58 +0200

    fixed count students with cbv view not template tags

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from students.models import Student
 
-
-class HomePageView(TemplateView):
+class HomePageView(ListView):
   template_name = 'home.html'
+  model = Student
+  context_object_name = 'online_students'
+
+  def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_company = self.request.user.company
+        context['online_students_count'] = self.get_queryset().filter(is_online=True,company=user_company).count()
+        context['offline_students_count'] = self.get_queryset().filter(is_online=False,company=user_company).count()
+        return context
+
+  def get_queryset(self):
+      return super().get_queryset()
+  
--- apps/students/templatetags/student_count.py
+++ apps/students/templatetags/student_count.py
@@ -1,20 +1,20 @@
-from django import template
-from students.models import Student
+# from django import template
+# from students.models import Student
 
-register = template.Library()
+# register = template.Library()
 
-@register.simple_tag
-def online_student_count():
-    """
-    Returns the count of online students.
-    """
-    return Student.objects.filter(is_online=True).count()
+# @register.simple_tag
+# def online_student_count():
+#     """
+#     Returns the count of online students.
+#     """
+#     return Student.objects.filter(is_online=True).count()
 
 
-@register.simple_tag
-def offline_student_count():
+# @register.simple_tag
+# def offline_student_count():
     
-    """
-    Returns the count of offline students.
-    """
-    return Student.objects.filter(is_online=False).count()
+#     """
+#     Returns the count of offline students.
+#     """
+#     return Student.objects.filter(is_online=False).count()
